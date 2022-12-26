3b928a74e6e5b33cf843375960ae2ef340e11a04
Swati Jindal <swatij@mindfiresolutions.com>
2022-12-26 19:38:24 +0530

    Added some functionality

--- blockchain/contracts/Admin.sol
+++ blockchain/contracts/Admin.sol
@@ -23,6 +23,7 @@ contract Admin is Ownable{
   }
     
   mapping(uint256 => userInfo) public userDetails;
+  mapping(uint256 => address[]) private validatorList;
    
   function addUser(address _userAddr, uint256 _userRole, string memory _ipfsURL) external onlyOwner(){
     unchecked{
@@ -31,31 +32,37 @@ contract Admin is Ownable{
     checkAddr(_userAddr);
     if(uint256(Role.Farmer) == _userRole){
       role = Role.Farmer;
-      userDetails[userId]._role = (role);
+      
     } else if(uint256(Role.Buyer) == _userRole){
       role = Role.Buyer;
-      userDetails[userId]._role = (role);
+      
     } else{
-       role = Role.Validator;
-       userDetails[userId]._role = (role);
+      role = Role.Validator;
+      validatorList[uint256(role)].push(_userAddr);
+      
     }
-
-     _addUser(_userAddr, userId, _ipfsURL);
+    userDetails[userId]._role = (role);
+    _addUser(_userAddr, userId, _ipfsURL);
   }
 
-    function _addUser(address _userAddr, uint256 _userId, string memory _ipfsURL) internal{
-      userDetails[_userId].userAddr = _userAddr;
-      userDetails[_userId].userId = _userId;
-      userDetails[_userId].ipfsURL = _ipfsURL;
-    }
+  function getValidatorList() external view returns(address[] memory){
+    return(validatorList[2]);
 
-    function checkAddr(address userAddr_) internal view{
-      for(uint256 i=1; i<=userId; ){
-        require(userAddr_ != userDetails[i].userAddr, "Already added user");
-        unchecked{
-          i++;
-        }
+  }
+
+  function _addUser(address _userAddr, uint256 _userId, string memory _ipfsURL) internal{
+    userDetails[_userId].userAddr = _userAddr;
+    userDetails[_userId].userId = _userId;
+    userDetails[_userId].ipfsURL = _ipfsURL;
+  }
+
+  function checkAddr(address userAddr_) internal view{
+    for(uint256 i=1; i<=userId; ){
+      require(userAddr_ != userDetails[i].userAddr, "Already added user");
+      unchecked{
+        i++;
       }
     }
+  }
 
 }
