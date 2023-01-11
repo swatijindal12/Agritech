d59350bfe50570a19abf060f71a78564e53381b0
sudhanshuk <sudhanshuk@mindfiresolutions.com>
2023-01-11 08:57:52 +0530

    removed dist

--- server/dist/controllers/authController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// route to      => api/v1/test
-exports.setup = (req, res, next) => {
-    return res.status(200).json({
-        success: true,
-        message: "working !"
-    });
-};
--- server/dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const router = express.Router();
-// Importing logic controllers
-const { setup } = require('../controllers/authController');
-// Route => api/v1/test
-router.route('/test').get(setup);
-module.exports = router;
