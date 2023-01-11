1c702af78d01a920a13b5061358d203736d08374
sudhanshuk <sudhanshuk@mindfiresolutions.com>
2023-01-11 20:44:20 +0530

    farms route added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Importing controllers
-const {validateData,createFarm,createFarmer,getFarmers,createCustomer} = require('../controllers/adminController');
+const {validateData,createFarm,createFarmer,getFarmers,createCustomer, getFarms} = require('../controllers/adminController');
 
 // Importing middleware to check authentication of routes
 const {isAuthenticatedUser,authorizeRoles} = require('../middlewares/auth')
@@ -16,7 +16,7 @@ router.route('/validate-data').post(isAuthenticatedUser,authorizeRoles('admin'),
 router.route('/farm').post(createFarm);
 
 // Get List of farms 
-// router.route('/farms').get(getFarms); //Working today
+router.route('/farms').get(isAuthenticatedUser,authorizeRoles('admin'), getFarms); //Working today
 
 // Insert farmer data into DB.
 router.route('/farmer').post(isAuthenticatedUser,authorizeRoles('admin'),createFarmer);
