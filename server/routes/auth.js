3109242cef3b157190de85973c7cf565ca216a3c
sudhanshuk <sudhanshuk@mindfiresolutions.com>
2023-01-11 19:26:28 +0530

    auth middleware for logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const {loginUser,verifyUser,logoutUser} = require('../controllers/authController');
 
 // Importing middleware
-// const {isAuthenticatedUser} = require('../middleware/auth')
+const {isAuthenticatedUser,authorizeRoles} = require('../middlewares/auth')
 
 // Route => api/v1/auth
 router.route('/login').post(loginUser);
 router.route('/verify').post(verifyUser);
-router.route('/logout').post(logoutUser);
+router.route('/logout').post(isAuthenticatedUser, logoutUser);
 
 module.exports = router;
