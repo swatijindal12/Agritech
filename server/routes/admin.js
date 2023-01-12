7cdbc3f3b3b0df7c48b4b33c74d4f1363f33628f
sudhanshuk <sudhanshuk@mindfiresolutions.com>
2023-01-13 01:02:37 +0530

    added rout for dashboard

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 // Importing controllers
-const {validateData,createFarm,createFarmer,getFarmers,createCustomer, getFarms} = require('../controllers/adminController');
+const { 
+    validateData,
+    createFarm,
+    createFarmer,
+    getFarmers,
+    createCustomer, 
+    getFarms, 
+    getCustomers,
+    getdashBoard
+} = require('../controllers/adminController');
 
 // Importing middleware to check authentication of routes
 const {isAuthenticatedUser,authorizeRoles} = require('../middlewares/auth')
@@ -16,7 +25,7 @@ router.route('/validate-data').post(isAuthenticatedUser,authorizeRoles('admin'),
 router.route('/farm').post(createFarm);
 
 // Get List of farms 
-router.route('/farms').get(isAuthenticatedUser,authorizeRoles('admin'), getFarms); //Working today
+router.route('/farms').get(isAuthenticatedUser,authorizeRoles('admin'), getFarms); 
 
 // Insert farmer data into DB.
 router.route('/farmer').post(isAuthenticatedUser,authorizeRoles('admin'),createFarmer);
@@ -26,6 +35,12 @@ router.route('/farmers').get(isAuthenticatedUser,authorizeRoles('admin'), getFar
 
 // Insert company data into DB.
 router.route('/customer').post(isAuthenticatedUser,authorizeRoles('admin'),createCustomer);
+
+// Get List of customers
+router.route('/customers').get(isAuthenticatedUser,authorizeRoles('admin'), getCustomers);
+
+// Get the data for Dashboard
+router.route('/dashboard').get(isAuthenticatedUser,authorizeRoles('admin'),getdashBoard)
 
 module.exports = router;
 
