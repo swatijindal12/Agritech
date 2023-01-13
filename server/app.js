0090b9f17e7f787c5e9af562dd1cf5ae406fba24
sudhanshuk <sudhanshuk@mindfiresolutions.com>
2023-01-13 11:16:40 +0530

    unhandled routes

--- server/app.js
+++ server/app.js
@@ -41,14 +41,14 @@ app.use('/api/v1/auth', auth);
 app.use('/api/v1/admin', adminRoutes)
 
 // Handling unhandled routes
-// app.use('*', (req, res) => {
-//     res.status(404).json({
-//       error:"Route not found",
-//       message:null,
-//       httpStatus: 404,
-//       data:null
-//     });
-// });
+app.use('*', (req, res) => {
+    res.status(404).json({
+      error:"Route not found",
+      message:null,
+      httpStatus: 404,
+      data:null
+    });
+});
 
 // Port on which Server run
 const PORT = process.env.PORT || 3000;
