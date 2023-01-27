99cfd7f1121cde0d0c0b527e8b58df6003fd5cec
sudhanshuk <sudhanshuk@mindfiresolutions.com>
2023-01-27 17:24:08 +0530

    payment service added

--- server/services/payment.js
+++ server/services/payment.js
@@ -2,6 +2,7 @@ const Razorpay = require("razorpay");
 const Payment = require("../models/payment");
 const Order = require("../models/order");
 const OrderItem = require("../models/orderItem");
+const Agreement = require("../models/agreements");
 const crypto = require("crypto");
 
 const instance = new Razorpay({
@@ -40,20 +41,6 @@ exports.createOrder = async (req) => {
   };
 
   try {
-    // Inserting Data into OrderItem Table
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < agreements[i].agreement_ids.length; j++) {
-        const unit_price = agreements[i].unit_price;
-
-        // Insert the Data in orderItem Table.
-        await OrderItem.create({
-          agreement_id: agreements[i].agreement_ids[j],
-          unit: req.body.unit,
-          price: unit_price,
-        });
-      }
-    }
-
     const options = {
       amount: Number(price * 100), // Total amount passed.
       currency: "INR",
@@ -63,12 +50,29 @@ exports.createOrder = async (req) => {
     console.log("Order :- ", order);
 
     // Inserting orderDetail in Order table
-    await Order.create({
+    const orderCreated = await Order.create({
       customer_id: req.user._id,
       razorpay_order_id: order.id,
       amount: price,
       currency: "INR",
     });
+    // const orderCreatedId = orderCreated.id;
+    console.log("orderCreatedId :- ", orderCreated);
+
+    // Inserting Data into OrderItem Table
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < agreements[i].agreement_ids.length; j++) {
+        const unit_price = agreements[i].unit_price;
+
+        // Insert the Data in orderItem Table.
+        await OrderItem.create({
+          order_id: orderCreated._id,
+          agreement_id: agreements[i].agreement_ids[j],
+          unit: req.body.unit,
+          price: unit_price,
+        });
+      }
+    }
 
     response.message = `Order created successful`;
     response.httpStatus = 200;
@@ -95,13 +99,6 @@ exports.paymentVerification = async (req) => {
   const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
     req.body;
 
-  console.log(
-    "Payment detail :- ",
-    razorpay_order_id,
-    razorpay_payment_id,
-    razorpay_signature
-  );
-
   const body = razorpay_order_id + "|" + razorpay_payment_id;
 
   const expectedSignature = crypto
@@ -111,14 +108,13 @@ exports.paymentVerification = async (req) => {
 
   const isAuthentic = expectedSignature === razorpay_signature;
 
-  console.log("isAuthentic :", isAuthentic);
   if (isAuthentic) {
     // Database comes here
 
-    Order.find({ razorpay_order_id })
+    Order.findOne({ razorpay_order_id })
       .then(async (order_id) => {
-        if (order_id.length > 0) {
-          const id = order_id[0]._id;
+        if (order_id) {
+          const id = order_id._id;
           await Payment.create({
             order_id: id,
             razorpay_order_id,
@@ -126,21 +122,30 @@ exports.paymentVerification = async (req) => {
             razorpay_payment_signature: razorpay_signature,
             payment_status: true,
           });
-          response.message = "Payment Successful";
-          response.httpStatus = 200;
+
+          // find the agreement and update its sold_status to true.
+          // Query in Order_Item table
+          const order_items = await OrderItem.find({ order_id: id });
+
+          for (let i = 0; i < order_items.length; i++) {
+            await Agreement.updateOne(
+              { _id: order_items[i].agreement_id },
+              { sold_status: true }
+            );
+          }
         } else {
-          response.message = "No order found";
+          response.error = "No order found";
           response.httpStatus = 404;
         }
       })
       .catch((err) => {
+        console.log(`error : ${err}`);
         response.message = "failed operation";
         response.httpStatus = 500;
       });
-
-    // res.redirect(
-    //   `http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`
-    // );
+    response.message = `Payment Successful`;
+    response.httpStatus = 200;
+    response.data = razorpay_payment_id;
   } else {
     response.error = "Payment failed";
     response.httpStatus = 500;
