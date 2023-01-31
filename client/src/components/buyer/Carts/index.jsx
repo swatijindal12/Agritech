c9b28d5124592ede54aabafbb9f3ad035dd57a79
Swati Jindal <swatij@mindfiresolutions.com>
2023-01-31 11:03:20 +0530

    removed default card holder name

--- client/src/components/buyer/Carts/index.jsx
+++ client/src/components/buyer/Carts/index.jsx
@@ -124,7 +124,7 @@ const Cart = () => {
           .catch(err => console.log("Error ", err));
       },
       prefill: {
-        name: "sudhanshu Kumar",
+        name: "",
         email: "sudhanshu.kumar@example.com",
         contact: "9999999999",
       },
