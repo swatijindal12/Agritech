925b585dea980eb1be00a477bccbe242f1a257ee
Neelam <neelamn@mindfiresolutions.com>
2023-02-02 00:12:05 +0530

    Login logo size updated

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -41,6 +41,12 @@ const CartNumber = styled.p`
   color: red;
 `;
 
+const Logo = styled.img`
+  height: 2.5rem;
+  width: 2.5rem;
+  object-fit: cover;
+`;
+
 const Navbar = ({ toggleSidebar }) => {
   const user = useSelector(store => store.auth.user);
   const cartItem = useSelector(store => store.cart.cart);
@@ -63,7 +69,7 @@ const Navbar = ({ toggleSidebar }) => {
 
   return (
     <Container>
-      <img src={LogoImg} />
+      <Logo src={LogoImg} />
       {user?.data?.role === "customer" && (
         <CartContainer>
           <CartNumber ref={cartNumberRef}>{cartItem.length}</CartNumber>
