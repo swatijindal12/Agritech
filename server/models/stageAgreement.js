e0f0c5d8653c437d71d2427b99d1b5db6f9dbc14
sudhanshuk <sudhanshuk@mindfiresolutions.com>
2023-02-09 16:44:23 +0530

    model for stageAgreement added

---
+++ server/models/stageAgreement.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+// User Model
+const stageAgreementSchema = new mongoose.Schema(
+  {
+    farmer_name: {
+      type: String,
+    },
+    farm_id: {
+      type: Number,
+    },
+    address: {
+      type: String,
+    },
+    start_date: {
+      type: String,
+      default: Date.now,
+    },
+    end_date: {
+      type: String,
+      default: Date.now,
+    },
+    crop: {
+      type: String,
+    },
+    area: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: [true, "Enter amount"],
+    },
+    farm_nft_id: {
+      type: String,
+    },
+    stage_status: {
+      type: Boolean,
+      default: true,
+    },
+    approval_status: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true, createdAt: "created_at", updatedAt: "updated_at" }
+);
+
+// Exporting userSchema as User
+module.exports = mongoose.model("StageAgreement", stageAgreementSchema);
