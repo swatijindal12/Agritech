8a0a3c3bd15f4cc8e507a87eaff9c6a83e0a1189
Neelam <neelamn@mindfiresolutions.com>
2023-02-14 18:54:19 +0530

    error bug resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import configStore from "./redux/store";
 import Routing from "./Routing";
 
-// External module exports.
-import Web3 from "web3";
-
 const { store, persistor } = configStore();
 
 const App = () => {
-  // Checking for web3 connection.
-
-  useEffect(() => {
-    const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-    console.log("web3 : ", web3);
-  }, []);
-
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
