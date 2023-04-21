176c898c8d8e20bc72f8090cc228da94c763b709
sudhanshuk <sudhanshuk@mindfiresolutions.com>
2023-04-21 18:28:27 +0530

    BUG: user registration verified

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,22 @@
 const mongoose = require("mongoose");
+const { logger } = require("../utils/logger");
+const { errorLog } = require("../utils/commonError");
+
+// Import the User model
+const User = require("../models/users");
+
+// Delete unverified users older than a specified time
+const deleteUnverifiedUsers = async () => {
+  const timeThreshold = new Date(Date.now() - 1 * 120 * 1000); // 1 minute ago
+  const result = await User.deleteMany({
+    is_verified: false,
+    created_at: { $lte: timeThreshold },
+  });
+  logger.log("info", `Deleted ${result.deletedCount} unverified users.`);
+};
+
+//Delete after 2 min
+setInterval(deleteUnverifiedUsers, 120 * 1000); // 2 minute
 
 // Connection using mongoose string
 const connectDatabase = () => {
@@ -9,6 +27,10 @@ const connectDatabase = () => {
         useUnifiedTopology: true,
       })
       .then((con) => {
+        logger.log(
+          "info",
+          `MongoDB Database connected with host ${con.connection.host}`
+        );
         console.log(
           `MongoDB Database connected with host ${con.connection.host}`
         );
