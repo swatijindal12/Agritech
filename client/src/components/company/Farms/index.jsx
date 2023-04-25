ae352331839864c7ad7fd8c12a672e4a2e546028
Swati Jindal <swatij@mindfiresolutions.com>
2023-04-25 16:56:35 +0530

    added search feature for farms & farmers

--- client/src/components/company/Farms/index.jsx
+++ client/src/components/company/Farms/index.jsx
@@ -5,6 +5,7 @@ import Title from "../../common/Title";
 import FilterIcon from "../../../assets/filter.svg";
 import Card from "./Card";
 import Filter from "./Filter";
+import Button from "../../common/Button";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
@@ -27,11 +28,48 @@ const CardsContainer = styled.div`
   }
 `;
 
+const InputContainer = styled(Flexbox)`
+  @media screen and (max-width: 990px) {
+    width: 100vw;
+    padding: 1rem;
+    margin: 0 auto 1rem;
+    flex-direction: column;
+    display: none;
+  }
+`;
+
+const SearchContainer = styled(Flexbox)`
+  @media screen and (max-width: 990px) {
+    width: 100vw;
+    padding: 1rem;
+    margin: 0 auto 1rem;
+    flex-direction: row;
+  }
+
+  @media screen and (min-width: 991px) {
+    display: none;
+  }
+`;
+
+const Input = styled.input`
+  padding: 1rem 2rem;
+  border: none;
+  width: 18rem;
+  border-radius: 24px;
+  background-color: #f5f5f5;
+
+  @media screen and (max-width: 990px) {
+    width: 70%;
+  }
+`;
+
 const Farms = () => {
   const [showFilter, setShowFilter] = useState(false);
   const [selectedFilter, setSelectedFilter] = useState({});
   const [farms, setFarms] = useState(null);
   const [newAddedIds, setNewAddedIds] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState("");
   const user = useSelector(store => store.auth.user);
 
   const toggleFilter = () => setShowFilter(!showFilter);
@@ -51,19 +89,23 @@ const Farms = () => {
   };
 
   const getList = () => {
+    setLoading(true);
     let queryString = "";
 
     if (selectedFilter.rating) {
       queryString += `sortOrder=${selectedFilter.rating}`;
     }
     axios
-      .get(`${process.env.REACT_APP_BASE_URL}/admin/farms?${queryString}`, {
-        headers: {
-          Authorization: "Bearer " + user?.data.token,
-        },
-      })
+      .get(
+        `${process.env.REACT_APP_BASE_URL}/admin/farms?${queryString}&search=${searchText}`,
+        {
+          headers: {
+            Authorization: "Bearer " + user?.data.token,
+          },
+        }
+      )
       .then(res => {
-        // console.log("res is", res.data.data);
+        setLoading(false);
         setFarms(res.data.data);
       })
       .catch(err => console.log("Error in fetching dashboard data ", err));
@@ -78,6 +120,21 @@ const Farms = () => {
     <Container>
       <Flexbox justify="space-between">
         <Title>Farms</Title>
+        <InputContainer margin="0 2rem">
+          <Input
+            type="text"
+            placeholder="Search by name, pin"
+            onChange={e => setSearchText(e.target.value)}
+          />
+          <Button
+            text={loading ? "...LOADING" : "SEARCH"}
+            margin="0 1rem"
+            onClick={() => {
+              getList();
+            }}
+            disabled={loading}
+          />
+        </InputContainer>
         <FilterContainer>
           <img src={FilterIcon} onClick={toggleFilter} />
           {showFilter && (
@@ -90,7 +147,22 @@ const Farms = () => {
           )}
         </FilterContainer>
       </Flexbox>
-      <br />
+      <SearchContainer margin="0 2rem">
+        <Input
+          type="text"
+          placeholder="Search by name, pin"
+          onChange={e => setSearchText(e.target.value)}
+        />
+        <Button
+          text={loading ? "...LOADING" : "SEARCH"}
+          margin="0 1rem"
+          onClick={() => {
+            getList();
+          }}
+          disabled={loading}
+        />
+      </SearchContainer>
+
       <CardsContainer>
         {farms?.map(item => {
           return (
